3be598e09a2d5d18fb6f52be8ceb8ac87926feaf
Marina Adams <bardakover@gmail.com>
2025-06-02 20:59:01 -0700

    Added changes to HTML and CSS, parts: City and Landscape

---
+++ src/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const header = document.getElementById("rotating-header");
+// const languages = [
+//   "Weather Report",
+//   "天气预报",           // Chinese
+//   "Informe del Clima", // Spanish
+//   "Прогноз погоды",     // Russian
+//   "मौसम की रिपोर्ट",     // Hindi
+// ];
+
+// let langIndex = 0;
+
+// setInterval(() => {
+// langIndex = (langIndex + 1) % languages.length;
+// header.textContent = languages[langIndex];
+// }, 3000); // каждые 3 секунды меняется заголовок
