80be39e001115f52018798a2d684f2840d0e8d52
Marina Adams <bardakover@gmail.com>
2025-06-04 23:07:40 -0700

    Refactored JS

--- src/index.js
+++ src/index.js
@@ -8,21 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
   cityInput.addEventListener("input", () => {
     displayedCityName.textContent = cityInput.value || "Seattle";
   });
+
+  resetButton.addEventListener("click", () => {
+    cityInput.value = "";
+    displayedCityName.textContent = "Seattle";
+  });
 });
-
-// tried to write a dynamic header, didnt finish
-// const header = document.getElementById("rotating-header");
-// const languages = [
-//   "Weather Report",
-//   "天气预报",           // Chinese
-//   "Informe del Clima", // Spanish
-//   "Прогноз погоды",     // Russian
-//   "मौसम की रिपोर्ट",     // Hindi
-// ];
-
-// let langIndex = 0;
-
-// setInterval(() => {
-// langIndex = (langIndex + 1) % languages.length;
-// header.textContent = languages[langIndex];
-// }, 3000); 
